8e381225011c736ad4fcf98a5107b7667053150c
QueenVol <159581003+QueenVol@users.noreply.github.com>
2025-11-20 22:28:48 -0500

    Camera Movement

---
+++ Juice Experiment/Assets/Script/Camera/CameraController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace TopDown.CameraControl
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private Transform playerTransform;
+        [SerializeField]private float displacement = 0.15f;
+        private float cameraZ = -10f;
+
+        private void Update()
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 cameraDisplacement = (mousePosition - playerTransform.position) * displacement;
+
+            Vector3 finalCameraPosition = playerTransform.position + cameraDisplacement;
+            finalCameraPosition.z = cameraZ;
+            transform.position = finalCameraPosition;
+        }
+    }
+}
